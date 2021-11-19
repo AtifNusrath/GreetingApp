c7d3b1905fddc7684013028959f5a2ae197e461e
atif00 <mohammedatif90@gmail.com>
2021-11-19 18:03:17 +0530

    Added: Returing JSON for diffrent Http methods

---
+++ src/main/java/com/example/demo/Controller/GreetingController.java
@@ -0,0 +1,26 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.Greeting;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping(value = { "/greeting", "/greeting/", "/greeting/home" })
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("greeting/{name}")
+    public Greeting greetings(@PathVariable String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
