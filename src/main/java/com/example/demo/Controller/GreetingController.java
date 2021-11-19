d6b903a1bf202f338ff7270282f7ae1a22d0bab1
atif00 <mohammedatif90@gmail.com>
2021-11-19 19:03:06 +0530

    Added: Greeting App with Greeting Message with first name and last name

--- src/main/java/com/example/demo/Controller/GreetingController.java
+++ src/main/java/com/example/demo/Controller/GreetingController.java
@@ -3,11 +3,14 @@ package com.example.demo.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Service.IGreetingService;
 import com.example.demo.dto.Greeting;
+import com.example.demo.dto.User;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -16,6 +19,9 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @Autowired
+    private IGreetingService greetingService;
+    
     @GetMapping(value = { "/greeting", "/greeting/", "/greeting/home" })
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
@@ -25,13 +31,14 @@ public class GreetingController {
     public Greeting greetings(@PathVariable String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
-    
-    @Autowired
-    private IGreetingService greetingService;
 
     @GetMapping("greeting/service")
     public Greeting greeting() {
         return greetingService.greetingMessage();
-
+    }
+    
+    @PostMapping("/greeting")
+    public String greetingMessage(@RequestBody User user) {
+        return "Hello "+user.firstName+" "+user.lastName;
     }
 }
